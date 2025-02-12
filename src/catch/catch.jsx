62a7a6650def5486a01fe730ba2aec663250a539
ReinnFall <jamestock18@gmail.com>
2025-02-11 18:01:36 -0700

    added jsx files for each html page

---
+++ src/catch/catch.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Catch() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>catch displayed here</div>
+    </main>
+  );
+}
---
+++ src/pokedex/pokedex.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Pokedex() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>pokedex displayed here</div>
+    </main>
+  );
+}
