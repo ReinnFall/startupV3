34eab3684b21fdc929c2af5a188e6861a90d293a
ReinnFall <jamestock18@gmail.com>
2025-02-11 17:58:35 -0700

    header and footer in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+    return (
+        <div>
+            <header>
+                <h1> Pokemon Catch</h1>
+                <nav>
+                    <a href="index.html">Home</a>
+                    <a href="catch.html">Catch</a>
+                    <a href="collection.html">Collection</a>
+                    <a href="pokedex.html">Pokedex</a>
+                </nav>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer>
+                <a href="https://github.com/ReinnFall/startupV3"> Github Link</a>
+            </footer>
+        </div>
+    );
 }
